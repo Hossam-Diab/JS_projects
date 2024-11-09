82ff5401510dea6d1fc39bbc8b72bc92168865b9
Hossam Diab <samdiab66666@gmail.com>
2024-11-09 14:36:58 +0200

    fixing a reset bug in filters

--- imageFilters_editor/main.js
+++ imageFilters_editor/main.js
@@ -1,88 +1,169 @@
-let saturation = document.getElementById('saturation');
+let saturate = document.getElementById('saturate');
 let contrast = document.getElementById('contrast');
 let brightness = document.getElementById('brightness');
 let sepia = document.getElementById('sepia');
 let grayscale = document.getElementById('grayscale');
-let _blur = document.getElementById('blur');
+let blur = document.getElementById('blur');
 let hueRotate = document.getElementById('hue-rotate');
-
-
 let upload = document.getElementById('upload');
 let download = document.getElementById('download');
-let img = document.getElementById('img');
+let reset = document.getElementById('reset');
+let img = document.querySelector('img');
 
-let reset = document.querySelector('span');
-let imgBox = document.querySelector('.imgBox');
+let imageBox = document.querySelector('.image');
 
-let canvas = document.getElementById('canvas');
-let context = canvas.getContext('2d');
+let filters = document.querySelectorAll('.filtersBox input');
 
-
-let resetValue =function(){
-        img.style.filter ='none';
-        saturation.value = '100';
-        contrast.value = '100';
-        brightness.value = '100';
-        sepia.value = '0';
-        grayscale.value = '0';
-        _blur.value = '0';
-        hueRotate.value = '0';
-
-}
+let canvas = document.querySelector('canvas');
+let ctx= canvas.getContext('2d');
 
 onload =function(){
-    download.style.display ='none';
-    reset.style.display ='none';
-    imgBox.style.display ='none' ;
+
+download.style.display ='none';
+reset.style.display ='none';
+imageBox.style.display='none';
+
+
+}
+
+function resetValues(){
+    // img.style.display ='block';
+    
+    
+    img.style.filter ='none';
+    saturate.value = '100';
+    contrast.value = '100';
+    brightness.value = '100';
+    sepia.value = '0';
+    blur.value = '0';
+    grayscale.value = '0';
+    hueRotate.value = '0';
+    ctx.filter ='none';
+    ctx.drawImage(img , 0 , 0 , canvas.width, canvas.height);
+}
+
+upload.onchange =function(){
+resetValues();
+    
+download.style.display ='block';
+reset.style.display ='block';
+imageBox.style.display='block';
+
+let file = new FileReader();
+file.readAsDataURL(upload.files[0]);
+
+file.onload = function(){
+    img.src = file.result;
+}
+
+img.onload =function(){
+    canvas.width = img.width;
+    canvas.height = img.height;
+
+    ctx.drawImage(img , 0 , 0 , canvas.width, canvas.height);
+    img.style.display ='none';
 }
 
 
-upload.onchange =function(){
-    resetValue();
-    download.style.display ='block';
-    reset.style.display ='block';
-    imgBox.style.display ='block' ;
 
-    let file = new FileReader();
-    file.readAsDataURL(upload.files[0]);
-    
-    file.onload =function(){
-         img.src = file.result ;
-        }
-    img.onload =function(){
-      canvas.width = img.width;
-      canvas.height = img.height;
-      
-        context.drawImage(img,0,0,canvas.width,canvas.height);
-        img.style.display ='none';
-    }
-
-
-    }
-    
-let filters = document.querySelectorAll("ul li input");
+}
 
 
 filters.forEach( filter => {
-    filter.addEventListener('input' , function(){
-        context.filter = `
-            saturate(${saturation.value}%)
-            contrast(${contrast.value}%)
-            brightness(${brightness.value}%)
-            sepia(${sepia.value}%)
-            grayscale(${grayscale.value})
-            blur(${_blur.value}px)
-            hue-rotate(${hueRotate.value}deg)   
-           
-        `
-        context.drawImage(img,0,0,canvas.width,canvas.height);
+
+    filter.addEventListener('input', function(){
+        ctx.filter=`
+        saturate(${saturate.value}%)
+        contrast(${contrast.value}%)
+        brightness(${brightness.value}%)
+        sepia(${sepia.value}%)
+        grayscale(${grayscale.value})
+        blur(${blur.value}px)
+        hue-rotate(${hueRotate.value}deg)
+        
+        `   
+        ctx.drawImage(img , 0 , 0 , canvas.width, canvas.height);
 
     })
+
 })
-   
+
 download.onclick = function(){
-    download.href = canvas.toDataURL("image/jpeg");
+    download.href = canvas.toDataURL("image");
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
